734df9fb86e5f04d1250e1479ae479b01b54e65c
Philipp Weinmann <philipp.weinmann71@gmail.com>
2024-11-21 17:39:34 +0100

    fixed bug

--- preprocessing/preprocessing_main.py
+++ preprocessing/preprocessing_main.py
@@ -1,9 +1,10 @@
 # %%
 import os
-new_directory = "/home/tu-philw/group/gecko/pweinmann/mip_local_unet"
+new_directory = "/home/tu-philw/group/gecko/pweinmann/mip_local_unet_v2/preprocessing/"
 os.chdir(new_directory)
 # %%
 from patient_loader import get_patients
 
 # %%
 patients = get_patients()
+# %%
