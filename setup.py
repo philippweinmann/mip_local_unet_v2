19b4c7d42ce519c709e452670f347d60027a701b
philipp weinmann <philipp.weinmann71@gmail.com>
2024-11-22 13:58:52 +0100

    ...

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from distutils.core import setup, find_packages
+
 setup(name='mip_unet_v2',
       version='0.1',
       description='My model for the MIP project',
       author='Philipp Weinmann',
       author_email='philipp.weinmann71@gmail.com',
-      packages=[],
+      packages=find_packages(),
+      install_requires=[
+        # Dependencies go here
+        'numpy',
+    ],
      )
