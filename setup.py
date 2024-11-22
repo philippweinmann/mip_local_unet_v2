c0ed4705d287b7f3b19cbb6eb7a966e19ea27fc1
philipp weinmann <philipp.weinmann71@gmail.com>
2024-11-22 13:50:47 +0100

    better structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(name='mip_unet_v2',
+      version='0.1',
+      description='My model for the MIP project',
+      author='Philipp Weinmann',
+      author_email='philipp.weinmann71@gmail.com',
+      packages=[],
+     )
