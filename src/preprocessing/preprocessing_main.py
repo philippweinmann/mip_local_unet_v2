9bbf5943c001e80708655a59195baa5ee9d06395
philipp weinmann <philipp.weinmann71@gmail.com>
2024-11-23 16:09:06 +0100

    ...

--- src/preprocessing/preprocessing_main.py
+++ src/preprocessing/preprocessing_main.py
@@ -7,7 +7,7 @@ from pathlib import Path
 # %%
 from src.patient_loader import get_patients
 import nibabel as nib
-from src.preprocessing.preprocessing_utils import resample_image, clip_scans
+from src.preprocessing.preprocessing_utils import resample_image, clip_scans, min_max_normalize, pad_image, divide_3d_image_into_patches
 from src.preprocessing import preprocessing_config
 
 # %%
@@ -26,5 +26,51 @@ original_spacing = image.header.get_zooms()[:3]
 resampled_image = resample_image(image.get_fdata(), target_spacing=preprocessing_config.TARGET_VOXEL_SPACING, original_spacing=original_spacing)
 resampled_mask = resample_image(mask.get_fdata(), target_spacing=preprocessing_config.TARGET_VOXEL_SPACING, original_spacing=original_spacing)
 # %%
-clipped_resampled_image = clip_scans(resampled_image, -600, 1000)
+# clip the values of the image
+clipped_resampled_image = clip_scans(resampled_image, preprocessing_config.CLIPPING_MIN, preprocessing_config.CLIPPING_MAX)
+# %%
+# min_max normalize the image
+min_max_normalized_image = min_max_normalize(clipped_resampled_image, preprocessing_config.CLIPPING_MIN, preprocessing_config.CLIPPING_MAX)
+
+# %%
+# Pad the image to prepare for patch extraction
+patch_size = 128
+padded_image = pad_image(min_max_normalized_image, patch_size)
+padded_mask = pad_image(resampled_mask, patch_size)
+
+# %%
+# divide the 3d image into patches
+block_shape = (patch_size, patch_size, patch_size) # isomorphic patches
+image_patches = divide_3d_image_into_patches(padded_image, block_shape)
+mask_patches = divide_3d_image_into_patches(padded_mask, block_shape)
+
+# %%
+# let's save the patches to disk
+output_dir = preprocessing_config.Output_dir
+img_patch_prefix = f"{patient_x.idx}_image_patch_"
+label_patch_prefix = f"{patient_x.idx}_label_patch_"
+
+for idx, (image_patch, mask_patch) in enumerate(zip(image_patches, mask_patches)):
+    img_patch_fp = output_dir / f"{img_patch_prefix}{idx}.nii.gz"
+    label_patch_fp = output_dir / f"{label_patch_prefix}{idx}.nii.gz"
+
+    img_patch = nib.Nifti1Image(image_patch, affine=image.affine)
+    label_patch = nib.Nifti1Image(mask_patch, affine=mask.affine)
+
+    nib.save(img_patch, img_patch_fp)
+    nib.save(label_patch, label_patch_fp)
+
+# %%
+# Let's visualize the first patch
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.imshow(image_patches[0][64], cmap="gray")
+plt.axis("off")
+plt.show()
+
+# %%
+plt.imshow(mask_patches[0][64], cmap="gray")
+plt.axis("off")
+plt.show()
 # %%
--- src/preprocessing/preprocessing_utils.py
+++ src/preprocessing/preprocessing_utils.py
@@ -1,7 +1,9 @@
 # %%
 from src.preprocessing import preprocessing_config
 import nibabel as nib
+import numpy as np
 from scipy.ndimage import zoom
+from skimage.util import view_as_blocks
 
 def get_voxel_spacing(image_fp):
     image = nib.load(image_fp)
@@ -22,3 +24,34 @@ def clip_scans(image, min_value, max_value):
     image[image > max_value] = max_value
 
     return image
+
+def min_max_normalize(image, min_value, max_value):
+    image = (image - min_value) / (max_value - min_value)
+
+    return image
+
+def pad_image(image, patch_size):
+    shape = image.shape
+
+    for idx, shape_dim in enumerate(shape):
+        rest = shape_dim % patch_size
+
+        if rest == 0:
+            print(f"no padding required for dim {idx}. Original image shape: {image.shape}")
+            continue
+
+        # we need to add that many slices
+        pad_length = patch_size - rest
+
+        # Pad the array with zeros along the first dimension, the zeroes are added at the end
+        pad_width = [(0, pad_length) if i == idx else (0, 0) for i in range(len(shape))]
+        padded_image = np.pad(image, pad_width, mode='constant')
+
+        assert np.all(padded_image[-pad_length:0] == 0)
+
+    return padded_image
+
+def divide_3d_image_into_patches(image_3d, block_shape):
+    patches = view_as_blocks(image_3d, block_shape).squeeze()
+
+    return patches
