a2d563834306bcc2ae59abbfe7d7ac42dfd9d413
philipp weinmann <philipp.weinmann71@gmail.com>
2024-11-23 20:12:07 +0100

    ...

--- src/configs/config.py
+++ src/configs/config.py
@@ -4,3 +4,4 @@ DATA_FOLDER = Path("/home/tu-philw/group/gecko/pweinmann/mip_local_unet_v2/data/
 
 # training configurations
 MULTIPLIER_HYPERPARAMETER = 100000
+NUMBER_OF_POS_FOR_BCE_TO_BE_1 = 200000
